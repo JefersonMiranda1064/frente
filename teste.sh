c4f4e6eefb6150af2cb4886b9b4e34bb79bb4e02
JefersonMiranda1064 <jefersonmirandadacruz@gmail.com>
2025-05-30 15:28:01 -0300

    teste baixar ok

--- teste.sh
+++ teste.sh
@@ -1,49 +1,28 @@
 #!/bin/bash
 
-perguntar_numlockx() {
-    read -rp "Você deseja realizar a instalação do NumLockX? (s/n): " resposta
-    resposta=$(echo "$resposta" | tr '[:upper:]' '[:lower:]' | xargs)
+obter_numero_cliente() {
+    read -p "Digite o Número do Cliente: " numero_cliente
 
-    if [ "$resposta" = "s" ]; then
-        echo "Verificando instalação do NumLockX..."
-
-        # Verifica se já está instalado
-        if dpkg -l | grep -q "^ii  numlockx "; then
-            echo "NumLockX já está instalado."
-        else
-            echo "Instalando NumLockX..."
-            sudo apt update
-            sudo apt install -y numlockx
-        fi
-
-        UsuarioReal=$(logname)
-        AutostartDir="/home/$UsuarioReal/.config/autostart"
-        DesktopFile="$AutostartDir/numlockx.desktop"
-
-        # Cria o diretório de autostart, se necessário
-        mkdir -p "$AutostartDir"
-
-        # Cria o arquivo .desktop se ele ainda não existir
-        if [ -f "$DesktopFile" ]; then
-            echo "O arquivo $DesktopFile já existe. Não será recriado."
-        else
-            cat <<EOL > "$DesktopFile"
-[Desktop Entry]
-Type=Application
-Exec=numlockx on
-Hidden=false
-NoDisplay=false
-X-GNOME-Autostart-enabled=true
-Name=NumLockX
-Comment=Enable NumLock at startup
-EOL
-            chown "$UsuarioReal:$UsuarioReal" "$DesktopFile"
-            echo "Arquivo de inicialização automática criado com sucesso: $DesktopFile"
-        fi
-    else
-        echo "Instalação do NumLockX ignorada."
+    if [[ -z "$numero_cliente" ]]; then
+        echo "Número do cliente não pode estar vazio."
+        return 1
     fi
+
+    # Faz o download dos arquivos do FTP
+    wget -p -m ftp://ftp.rpinfo.com.br/suporte/clientes/"$numero_cliente"/ --ftp-user=jeferson.miranda --ftp-password=rp2019@@
+
+    # Move os arquivos baixados para a pasta frente
+    mv /home/rpdv/ftp.rpinfo.com.br/suporte/clientes/"$numero_cliente"/ /home/rpdv/frente/
+
+    # Copia os arquivos principais
+    cp /home/rpdv/frente/"$numero_cliente"/* /home/rpdv/frente/
+    cp /home/rpdv/frente/"$numero_cliente"/clisitef/* /home/rpdv/frente/
+    cp /home/rpdv/frente/"$numero_cliente"/clisitef/* /home/rpdv/frente/so/
+
+    # Limpa os arquivos temporários
+    rm -rf /home/rpdv/ftp.rpinfo.com.br
+    rm -rf /home/rpdv/frente/"$numero_cliente"/
 }
-perguntar_numlockx
+obter_numero_cliente
 
 #wget -p -m ftp://ftp.rpinfo.com.br/suporte/jeferson/wp/ --ftp-user=jeferson.miranda --ftp-password=rp2019@@
